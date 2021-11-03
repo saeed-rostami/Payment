6fb4694645b45774657f9aba18c87ae07f627882
saeed-rostami <60143387+saeed-rostami@users.noreply.github.com>
2021-11-03 20:08:27 +0330

    Update index.blade.php

--- Modules/Product/Resources/views/index.blade.php
+++ Modules/Product/Resources/views/index.blade.php
@@ -10,39 +10,8 @@
                 @csrf
                 <button class="btn" type="submit">خرید</button>
             </form>
-            {{--<button--}}
-                {{--onclick="purchase({{$product->id}})"--}}
-                {{--data-url="{{route('purchase' , $product->id)}}"--}}
-                {{--data-id="{{$product->id}}"--}}
-            {{-->--}}
-                {{--خرید--}}
-            {{--</button>--}}
+           
         </div>
     @endforeach
 @endsection
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"
-        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
-<script>
-    // $(document).ready(function () {
-
-
-        function purchase(id) {
-            let element = $('#' + id);
-            let url = element.attr('data-url');
-            $.ajax({
-                url: "api/purchase/"+id,
-                type: 'POST',
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                success: function (response) {
-                    if (response.status) {
-                        console.log(response)
-                    }
-                },
-            });
-        }
-    // });
-</script>
